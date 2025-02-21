0133e34182d079485e0ef58e166a9bce6db7285a
Jay Carlton <classicalcrazyartist@gmail.com>
2025-02-21 14:00:27 -0400

    Updating Player Movement with Animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,74 @@
 extends CharacterBody2D
 
-const accel =1500
-const friction =600
-const speed =100
 
-var input = Vector2.ZERO
+const speed =30
+var current_dir ="none"
+
 
 func _physics_process(delta):
 	player_movement(delta)
 
 
-func get_input():
-	input.x =int(Input.is_action_pressed("ui_right"))- int(Input.is_action_pressed("ui_left"))
-	input.y =int(Input.is_action_pressed("ui_down"))- int(Input.is_action_pressed("ui_up"))
-	return input.normalized()
-
-
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
 
 func player_movement(delta):
-	input = get_input()
+	
 #personal movement
-
-
-	if input != Vector2.ZERO:
-		#if velocity.length() > (friction * delta):
-			#velocity -= velocity.normalized() * (friction*delta)
-		#else:
-			#velocity = Vector2.ZERO
-	#else:
-		velocity+=(input * speed)
-		velocity = velocity.limit_length(speed)
-		
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_animation(1)
+		velocity.x = speed
+		velocity.y =0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_animation(1)
+		velocity.x = -speed
+		velocity.y =0
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_animation(1)
+		velocity.x = 0
+		velocity.y =-speed
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_animation(1)
+		velocity.x = 0
+		velocity.y =speed
 	else:
-		velocity = input
-		
+		velocity.x = 0
+		velocity.y =0
+		play_animation(0)
 		
 		
 	move_and_slide()
+	
+func play_animation(movement):
+	var dir = current_dir
+	var animation = $AnimatedSprite2D
+	
+	if dir== "right":
+		animation.flip_h=false
+		if movement ==1:
+			animation.play("side_walk")
+		elif movement ==0:
+			animation.play("side_idle")
+	elif dir== "left":
+		animation.flip_h=true
+		if movement ==1:
+			animation.play("side_walk")
+		elif movement ==0:
+			animation.play("side_idle")
+	elif dir== "up":
+		animation.flip_h=true
+		if movement ==1:
+			animation.play("back_walk")
+		elif movement ==0:
+			animation.play("back_idle")
+	elif dir== "down":
+		animation.flip_h=true
+		if movement ==1:
+			animation.play("front_walk")
+		elif movement ==0:
+			animation.play("front_idle")
+		
